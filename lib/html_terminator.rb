8d0573e3a12a7a6141e43242acb0470603fbb8c5
Steel Fu <steelfu@gmail.com>
2013-11-12 17:43:50 -0800

    check if table exists before trying to grab column names

--- lib/html_terminator.rb
+++ lib/html_terminator.rb
@@ -18,34 +18,37 @@ module HtmlTerminator
     def terminate_html(*args)
       class_attribute :html_terminator_fields
 
-      # By default all fields are to be seen by the terminator
-      self.html_terminator_fields = self.columns.inject([]) do |list, col|
-        if col.type == :string or col.type == :text
-          list << col.name.to_sym
+      # Table may not exist yet when schema is initially getting loaded
+      if self.table_exists?
+        # By default all fields are to be seen by the terminator
+        self.html_terminator_fields = self.columns.inject([]) do |list, col|
+          if col.type == :string or col.type == :text
+            list << col.name.to_sym
+          end
+
+          list
         end
 
-        list
-      end
-
-      if args.length == 1
-        if args[0].is_a?(Symbol)
+        if args.length == 1
+          if args[0].is_a?(Symbol)
+            self.html_terminator_fields = args
+          elsif args[0].is_a?(Object)
+            self.html_terminator_fields -= (args[0][:except] || [])
+          end
+        elsif args.length > 1
           self.html_terminator_fields = args
-        elsif args[0].is_a?(Object)
-          self.html_terminator_fields -= (args[0][:except] || [])
         end
-      elsif args.length > 1
-        self.html_terminator_fields = args
-      end
 
-      unless self.html_terminator_fields.empty?
-        # sanitize writes
-        before_validation :terminate_html
+        unless self.html_terminator_fields.empty?
+          # sanitize writes
+          before_validation :terminate_html
 
-        # sanitize reads
-        self.html_terminator_fields.each do |attr|
-          define_method "#{attr}" do |*args|
-            # sanitize it
-            HtmlTerminator.sanitize super(*args)
+          # sanitize reads
+          self.html_terminator_fields.each do |attr|
+            define_method "#{attr}" do |*args|
+              # sanitize it
+              HtmlTerminator.sanitize super(*args)
+            end
           end
         end
       end
